eec2c7fa4c7fd4358993315c2c535858df873cfd
thelonggoodbuy <thelonggoodbuy1991@gmail.com>
2025-02-15 17:49:29 +0200

    add processing existing subscription

--- src/infrastructure/database/repositories/subscriptions.py
+++ src/infrastructure/database/repositories/subscriptions.py
@@ -39,7 +39,11 @@ class SubscriptionRepository(BaseSubscribtionRepository, IAlchemyRepository):
         
     async def update_subscription_by_subscription_id_and_period(self, subscription_id: int, period: PeriodTypeEnum):
         subscription_obj = await self.return_subscribtion_by_id(id=subscription_id)
-        subscription_updated_period = await self.get_updated_period(period)
+        if not subscription_obj.expiration_date or subscription_obj.expiration_date < datetime.now(timezone.utc):
+            subscription_updated_period = await self.new_updated_period(period)
+        else:
+            subscription_updated_period = await self.add_to_updated_period(period, finish_date=subscription_obj.expiration_date)
+
         subscription_obj.expiration_date = subscription_updated_period
         subscription_obj.is_active = True
         self._session.add(subscription_obj)
@@ -47,7 +51,19 @@ class SubscriptionRepository(BaseSubscribtionRepository, IAlchemyRepository):
         return subscription_obj
 
 
-    async def get_updated_period(self, period: PeriodTypeEnum):
+    async def add_to_updated_period(self, period: PeriodTypeEnum, finish_date):
+        match period:
+            case PeriodTypeEnum.WEEK:
+                return await self.handle_week(finish_date)
+            case PeriodTypeEnum.MONTH:
+                return await self.handle_month(finish_date)
+            case PeriodTypeEnum.YEAR:
+                return await self.handle_year(finish_date)
+            case _:
+                raise ValueError(f"Unsupported period type: {period}")
+
+
+    async def new_updated_period(self, period: PeriodTypeEnum):
         match period:
             case PeriodTypeEnum.WEEK:
                 return await self.handle_week()
@@ -59,20 +75,31 @@ class SubscriptionRepository(BaseSubscribtionRepository, IAlchemyRepository):
                 raise ValueError(f"Unsupported period type: {period}")
 
 
-    async def handle_week(self) -> datetime:
-        now = datetime.now(timezone.utc)
-        tomorrow = (now + timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0) 
-        return tomorrow + timedelta(weeks=1)
+    async def handle_week(self, finish_date=None) -> datetime:
+        if finish_date:
+            result = finish_date + timedelta(weeks=1)
+        else:
+            now = datetime.now(timezone.utc)
+            tomorrow = (now + timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0) 
+            result = tomorrow + timedelta(weeks=1)
+        return result
         
 
+    async def handle_month(self, finish_date=None) -> datetime:
+        if finish_date:
+            result = finish_date + relativedelta(months=1)
+        else:
+            now = datetime.now(timezone.utc)
+            tomorrow = (now + timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
+            result = tomorrow + relativedelta(months=1)
+        return result
 
-    async def handle_month(self) -> datetime:
-        now = datetime.now(timezone.utc)
-        tomorrow = (now + timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
-        return tomorrow + relativedelta(months=1)
 
-
-    async def handle_year(self) -> datetime:
-        now = datetime.now(timezone.utc)
-        tomorrow = (now + timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
-        return tomorrow + relativedelta(years=1)
+    async def handle_year(self, finish_date=None) -> datetime:
+        if finish_date:
+            result = finish_date + relativedelta(months=1)
+        else:
+            now = datetime.now(timezone.utc)
+            tomorrow = (now + timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
+            result = tomorrow + relativedelta(years=1)
+        return result
