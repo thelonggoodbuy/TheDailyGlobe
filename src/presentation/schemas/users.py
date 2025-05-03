2128590393a92df59ce6176475d1c3acfca9e416
thelonggoodbuy <thelonggoodbuy1991@gmail.com>
2025-05-03 18:20:08 +0300

    fix:debug_expiration_date_9

--- src/presentation/schemas/users.py
+++ src/presentation/schemas/users.py
@@ -180,7 +180,7 @@ class LogOutRequestData(BaseModelWithCamelCase):
 class UserDataSchema(BaseModel):
     email: str
     is_registered_throw_google: Optional[bool] = Field(default=False, alias='isRegisteredThrowGoogle')
-    expiration_date: Optional[str] = Field(default=None, alias='expirationDate')
+    expiration_date: Optional[str] = Field(alias='expirationDate')
     is_active_subscription: Optional[bool] = Field(default=False, alias='isActiveSubscription')
 
 
